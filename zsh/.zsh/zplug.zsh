8b9e8aeeeb37ea031dc330b2c316c0b01ebf88db
uodna <uodna0@gmail.com>
2018-06-24 00:44:07 +0900

    [zsh] fix indentation

--- zsh/.zsh/zplug.zsh
+++ zsh/.zsh/zplug.zsh
@@ -1,12 +1,11 @@
-    zplug "zplug/zplug", hook-build:"zplug --self-manage"
-    zplug "zsh-users/zsh-completions"
-    zplug "zsh-users/zsh-autosuggestions"
-    zplug "zsh-users/zsh-syntax-highlighting", defer:2
-    zplug "zsh-users/zsh-history-substring-search", defer:3
-    zplug "mafredri/zsh-async", from:github
-    # zplug "changyuheng/fz", defer:1
-    zplug "rupa/z", use:z.sh
-    # zplug "changyuheng/zsh-interactive-cd"
-    zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
-    zplug "joel-porquet/zsh-dircolors-solarized"
-
+zplug "zplug/zplug", hook-build:"zplug --self-manage"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "zsh-users/zsh-history-substring-search", defer:3
+zplug "mafredri/zsh-async", from:github
+# zplug "changyuheng/fz", defer:1
+zplug "rupa/z", use:z.sh
+# zplug "changyuheng/zsh-interactive-cd"
+zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
+zplug "joel-porquet/zsh-dircolors-solarized"
